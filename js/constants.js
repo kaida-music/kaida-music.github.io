6e2d9e14a873df65324ff8373020d013a80af4f1
pepepepepei <jumpei.yamada.nagoya@gmail.com>
2025-11-05 22:52:59 +0900

    first commit

---
+++ js/constants.js
@@ -0,0 +1,13 @@
+const CONSTANTS = {
+  API_URL:
+    "https://script.google.com/macros/s/AKfycbwOJxx-lPD8qJi9BHn_qV-yptACPXlJB68ylP2ORQMSPG10NqFP71kSXJ-YlHmh3Njx/exec",
+  YOUTUBE_API_URL: "https://www.youtube.com/iframe_api",
+  UPDATE_INTERVAL: 250,
+  ERROR_RETRY_DELAY: 5000,
+  THUMB_BASE_URL: "https://img.youtube.com/vi_webp/",
+  YOUTUBE_WATCH_URL: "https://youtu.be/",
+  TWITTER_INTENT_URL:
+    "https://x.com/intent/tweet?ref_src=twsrc%5Etfw%7Ctwcamp%5Ebuttonembed%7Ctwterm%5Eshare%7Ctwgr%5E",
+};
+
+export default CONSTANTS;
